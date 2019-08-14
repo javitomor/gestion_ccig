ee43b9cd6f0210cbf542a6ecd9cbaf00f56fbfad
javiermoreno <javiermoreno@192.168.1.12>
2019-08-14 19:03:39 -0300

    Primer commit

---
+++ src/main/java/mil/candes/core/GestionCcigApplication.java
@@ -0,0 +1,13 @@
+package mil.candes.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCcigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCcigApplication.class, args);
+	}
+
+}
